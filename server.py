800dce632942488cf1b21fbbd26f0375961d2630
Madhav Jha <madhavjha.cse@gmail.com>
2026-04-01 14:05:25 -0400

    Updated to render html

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 """ Server for Emotion Detection Application """
 
-from flask import Flask, request
+from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
@@ -24,6 +24,14 @@ def get_emotion_detector():
     return (f"For the given statement, the system response is 'anger': {anger}, "
             f"'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. "
             f"The dominant emotion is {dominant_emotion}.")
+
+@app.route("/")
+def render_index_page():
+    '''
+    This function initiates the rendering of the main application
+    page over the Flask channel.
+    '''
+    return render_template('index.html')            
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
